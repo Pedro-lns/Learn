840f57f2e3147a334f6794291a96e0eb4d29b6a9
Pedro <pedro.lns.dev@gmail.com>
2026-02-23 21:12:07 -0300

    Enums

--- src/Enums/Exercicios/Conta.java
+++ src/Enums/Exercicios/Conta.java
@@ -4,6 +4,4 @@ public enum Conta {
     ATIVA,
     BLOQUEADA,
     ENCERRADA;
-
-
 }
--- src/Enums/Exercicios/ContaMain.java
+++ src/Enums/Exercicios/ContaMain.java
@@ -1,7 +1,55 @@
 package Enums.Exercicios;
 
+import java.util.Random;
+
 public class ContaMain {
     public static void main(String[] args){
-        
+        ContaMain cm = new ContaMain();
+
+        Conta statusConta = Conta.ATIVA;
+        cm.info(statusConta);
+
+        statusConta = cm.setStatus();
+        cm.info(statusConta);
+    }
+
+    public void info(Conta statusConta){
+        switch (statusConta) {
+            case ATIVA:
+                System.out.println("-=".repeat(30));
+                System.out.println("Status: " + statusConta);
+                System.out.println("Usuário logado!");
+                System.out.println("-=".repeat(30));
+                break;
+
+            case BLOQUEADA:
+                System.out.println("-=".repeat(30));
+                System.out.println("Status: " + statusConta);
+                System.out.println("Usário bloqueado, impossivel acessar conta!");
+                System.out.println("-=".repeat(30));
+                break;
+
+            case ENCERRADA:
+                System.out.println("-=".repeat(30));
+                System.out.println("Status: " + statusConta);
+                System.out.println("Usuário não faz mais parte do sistema!");
+                System.out.println("-=".repeat(30));
+                break;
+        }
+    }
+
+    public Conta setStatus(){
+        Random rand = new Random();
+
+        int temp = rand.nextInt(3);
+        Conta statusCont = null;
+
+        switch (temp){
+            case 2 -> statusCont = Conta.ENCERRADA;
+            case 1 -> statusCont = Conta.ATIVA;
+            case 0 -> statusCont = Conta.BLOQUEADA;
+        }
+
+        return statusCont;
     }
 }
